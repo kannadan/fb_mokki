af56c2fb0033d3abf03540348f784c884f8b4ad0
kannadan <joonas.sou@gmail.com>
2023-06-02 22:12:51 +0300

    added sijoitukset command

--- bot.py
+++ bot.py
@@ -9,6 +9,7 @@ import os.path
 
 from google.oauth2 import service_account
 from googleapiclient.discovery import build
+import requests
 
 load_dotenv()
 
@@ -31,6 +32,7 @@ service = build('sheets', 'v4', credentials=creds)
 spreadsheet_id = os.getenv('sheet_id')
 range_name = "'Infoo ja osallistujat'!C66:Q86"
 bed_range = "'Infoo ja osallistujat'!C110:G127"
+score_range = "'Infoo ja osallistujat'!AI46:AO63"
 
 
 
@@ -50,6 +52,21 @@ def find_index_of_name(list_of_lists, name):
             return index
     return -1  
 
+def find_player(players, name):
+    for player in players:
+        if player['name'].lower() == name.lower().strip('<>'):
+            return player
+    return -1  
+
+def get_players():
+    try:
+        response = requests.get("https://api.frisbeer.win/API/players/")
+        response.raise_for_status()  # Raise an exception for non-2xx status codes
+        return response.json()  # Return the response content parsed as JSON
+    except requests.exceptions.RequestException as e:
+        print(f"Error: {e}")
+        return None
+
 async def mokki(update: Update, context: ContextTypes.DEFAULT_TYPE):
     args = context.args
     if(signup_is_live() != True):
@@ -64,8 +81,6 @@ async def mokki(update: Update, context: ContextTypes.DEFAULT_TYPE):
         reply_markup = InlineKeyboardMarkup(keyboard)
         context.user_data['user_param'] = args
         await update.message.reply_text("Haluatko varmasti lähteä mökille? Ilmoittautuminen on sitovaa", reply_markup=reply_markup)
-        # response = ' '.join(args) + ' on menossa mökille'
-        # await context.bot.send_message(chat_id=update.effective_chat.id, text=response)
 
 async def maksettu(update: Update, context: ContextTypes.DEFAULT_TYPE):
     args = context.args
@@ -80,7 +95,6 @@ async def maksettu(update: Update, context: ContextTypes.DEFAULT_TYPE):
             range=range_name
         ).execute()
     names = signups.get('values', [])
-    cells = len(names)
     names = [cell for cell in names if cell]
     names_only = [cell[0] for cell in names if cell]
     name = ' '.join(args)
@@ -122,6 +136,35 @@ async def maksettu(update: Update, context: ContextTypes.DEFAULT_TYPE):
             body=request_body
         ).execute()
     await context.bot.send_message(chat_id=update.effective_chat.id, text="Kiitos maksusta. {} nukkuu sijalla {}".format(name, bed_count + 1))
+
+async def sijoitukset(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    signups = service.spreadsheets().values().get(
+            spreadsheetId=spreadsheet_id,
+            range=score_range
+        ).execute()
+    names = signups.get('values', [])
+    names = [cell for cell in names if cell]
+    names_only = [cell[0] for cell in names if cell]
+    players = get_players()
+    result = []
+    for index, name in enumerate(names_only):
+        player = find_player(players, name)
+        original = 0
+        try:
+            original = int(names[index][5])
+        except ValueError:
+            pass
+        if player == -1:
+            result.append({'name': name, 'score': 'ei löytynyt', 'change': original * - 1})
+        else:
+            player['change'] = player['score'] - original
+            result.append(player)
+    result = sorted(result, key=lambda x: x['change'], reverse=True)
+    return_text = ''
+    for index, player in enumerate(result):
+        return_text += '{}. {} - {} ({})\n'.format(index + 1, player['name'], player['score'], player['change'])
+    await context.bot.send_message(chat_id=update.effective_chat.id, text=return_text)
+
     
             
 
@@ -172,6 +215,7 @@ if __name__ == '__main__':
     mokki_handler = CommandHandler('mokille', mokki)
     mokki_reply_handler = CallbackQueryHandler(button)
     maksu_handler = CommandHandler('maksettu', maksettu)
+    maksu_handler = CommandHandler('sijoitukset', sijoitukset)
 
     application.add_handler(mokki_handler)
     application.add_handler(mokki_reply_handler)
